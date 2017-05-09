e41d041e0e9f20496e2641d0beb9ea12bbfb56e0
Nikita <vishniakovcz@gmail.com>
2017-05-09 21:36:12 +0300

    Update config_db.php

--- admin/config_db.php
+++ admin/config_db.php
@@ -3,9 +3,9 @@
 //        Database data
     define('DB_DRIVER', 'mysqli');
     define('DB_HOSTNAME', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', 'vertrigo');
-    define('DB_DATABASE', 'welcome');
+    define('DB_USERNAME', 'u0263444_default');
+    define('DB_PASSWORD', '920!mZaD');
+    define('DB_DATABASE', 'u0263444_default');
     define('REDIRECT_JS', 'https://hotelwelcome.ru/admin/booking_page.php?id=');
     
 ?>
